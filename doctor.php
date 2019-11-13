a7c2114629b11cd5206ca41a07c8bb53135d5334
rukhsana27 <36368163+rukhsana27@users.noreply.github.com>
2019-11-13 13:18:38 +0600

    First commit

---
+++ doctor.php
@@ -0,0 +1,272 @@
+<?php
+    include('common/html_head.php');
+?>
+  <body>
+    
+<?php
+      include('common/nav.php');
+?>
+    <!-- END nav -->
+    
+    <div class="hero-wrap" style="background-image: url('images/bg_6.jpg'); background-attachment:fixed;">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+          <div class="col-md-8 ftco-animate text-center">
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Doctor</span></p>
+            <h1 class="mb-3 bread">Well Experienced Doctors</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="ftco-section">
+    	<div class="container">
+        <div class="row">
+        	<div class="col-md-6 col-lg-3 ftco-animate">
+	          <div class="block-2">
+	            <div class="flipper">
+	              <div class="front" style="background-image: url(images/doctor-1.jpg);">
+	                <div class="box">
+	                  <h2>Aldin Powell</h2>
+	                  <p>Neurologist</p>
+	                </div>
+	              </div>
+	              <div class="back">
+	                <!-- back content -->
+	                <blockquote>
+	                  <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem&rdquo;</p>
+	                </blockquote>
+	                <div class="author d-flex">
+	                  <div class="image mr-3 align-self-center">
+	                    <div class="img" style="background-image: url(images/doctor-1.jpg);"></div>
+	                  </div>
+	                  <div class="name align-self-center">Aldin Powell <span class="position">Neurologist</span></div>
+	                </div>
+	              </div>
+	            </div>
+	          </div> <!-- .flip-container -->
+	        </div>
+	        <div class="col-md-6 col-lg-3 ftco-animate">
+	          <div class="block-2">
+	            <div class="flipper">
+	              <div class="front" style="background-image: url(images/doctor-2.jpg);">
+	                <div class="box">
+	                  <h2>Aldin Powell</h2>
+	                  <p>Pediatrician</p>
+	                </div>
+	              </div>
+	              <div class="back">
+	                <!-- back content -->
+	                <blockquote>
+	                  <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem&rdquo;</p>
+	                </blockquote>
+	                <div class="author d-flex">
+	                  <div class="image mr-3 align-self-center">
+	                    <div class="img" style="background-image: url(images/doctor-2.jpg);"></div>
+	                  </div>
+	                  <div class="name align-self-center">Aldin Powell <span class="position">Pediatrician</span></div>
+	                </div>
+	              </div>
+	            </div>
+	          </div> <!-- .flip-container -->
+	        </div>
+	        <div class="col-md-6 col-lg-3 ftco-animate">
+	          <div class="block-2">
+	            <div class="flipper">
+	              <div class="front" style="background-image: url(images/doctor-3.jpg);">
+	                <div class="box">
+	                  <h2>Aldin Powell</h2>
+	                  <p>Ophthalmologist</p>
+	                </div>
+	              </div>
+	              <div class="back">
+	                <!-- back content -->
+	                <blockquote>
+	                  <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem&rdquo;</p>
+	                </blockquote>
+	                <div class="author d-flex">
+	                  <div class="image mr-3 align-self-center">
+	                    <div class="img" style="background-image: url(images/doctor-3.jpg);"></div>
+	                  </div>
+	                  <div class="name align-self-center">Aldin Powell <span class="position">Ophthalmologist</span></div>
+	                </div>
+	              </div>
+	            </div>
+	          </div> <!-- .flip-container -->
+	        </div>
+	        <div class="col-md-6 col-lg-3 ftco-animate">
+	          <div class="block-2">
+	            <div class="flipper">
+	              <div class="front" style="background-image: url(images/doctor-4.jpg);">
+	                <div class="box">
+	                  <h2>Aldin Powell</h2>
+	                  <p>Pulmonologist</p>
+	                </div>
+	              </div>
+	              <div class="back">
+	                <!-- back content -->
+	                <blockquote>
+	                  <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem&rdquo;</p>
+	                </blockquote>
+	                <div class="author d-flex">
+	                  <div class="image mr-3 align-self-center">
+	                    <div class="img" style="background-image: url(images/doctor-4.jpg);"></div>
+	                  </div>
+	                  <div class="name align-self-center">Aldin Powell <span class="position">Pulmonologist</span></div>
+	                </div>
+	              </div>
+	            </div>
+	          </div> <!-- .flip-container -->
+	        </div>
+
+	        <div class="col-md-6 col-lg-3 ftco-animate">
+	          <div class="block-2">
+	            <div class="flipper">
+	              <div class="front" style="background-image: url(images/doctor-1.jpg);">
+	                <div class="box">
+	                  <h2>Aldin Powell</h2>
+	                  <p>Neurologist</p>
+	                </div>
+	              </div>
+	              <div class="back">
+	                <!-- back content -->
+	                <blockquote>
+	                  <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem&rdquo;</p>
+	                </blockquote>
+	                <div class="author d-flex">
+	                  <div class="image mr-3 align-self-center">
+	                    <div class="img" style="background-image: url(images/doctor-1.jpg);"></div>
+	                  </div>
+	                  <div class="name align-self-center">Aldin Powell <span class="position">Neurologist</span></div>
+	                </div>
+	              </div>
+	            </div>
+	          </div> <!-- .flip-container -->
+	        </div>
+	        <div class="col-md-6 col-lg-3 ftco-animate">
+	          <div class="block-2">
+	            <div class="flipper">
+	              <div class="front" style="background-image: url(images/doctor-2.jpg);">
+	                <div class="box">
+	                  <h2>Aldin Powell</h2>
+	                  <p>Pediatrician</p>
+	                </div>
+	              </div>
+	              <div class="back">
+	                <!-- back content -->
+	                <blockquote>
+	                  <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem&rdquo;</p>
+	                </blockquote>
+	                <div class="author d-flex">
+	                  <div class="image mr-3 align-self-center">
+	                    <div class="img" style="background-image: url(images/doctor-2.jpg);"></div>
+	                  </div>
+	                  <div class="name align-self-center">Aldin Powell <span class="position">Pediatrician</span></div>
+	                </div>
+	              </div>
+	            </div>
+	          </div> <!-- .flip-container -->
+	        </div>
+	        <div class="col-md-6 col-lg-3 ftco-animate">
+	          <div class="block-2">
+	            <div class="flipper">
+	              <div class="front" style="background-image: url(images/doctor-3.jpg);">
+	                <div class="box">
+	                  <h2>Aldin Powell</h2>
+	                  <p>Ophthalmologist</p>
+	                </div>
+	              </div>
+	              <div class="back">
+	                <!-- back content -->
+	                <blockquote>
+	                  <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem&rdquo;</p>
+	                </blockquote>
+	                <div class="author d-flex">
+	                  <div class="image mr-3 align-self-center">
+	                    <div class="img" style="background-image: url(images/doctor-3.jpg);"></div>
+	                  </div>
+	                  <div class="name align-self-center">Aldin Powell <span class="position">Ophthalmologist</span></div>
+	                </div>
+	              </div>
+	            </div>
+	          </div> <!-- .flip-container -->
+	        </div>
+	        <div class="col-md-6 col-lg-3 ftco-animate">
+	          <div class="block-2">
+	            <div class="flipper">
+	              <div class="front" style="background-image: url(images/doctor-4.jpg);">
+	                <div class="box">
+	                  <h2>Aldin Powell</h2>
+	                  <p>Pulmonologist</p>
+	                </div>
+	              </div>
+	              <div class="back">
+	                <!-- back content -->
+	                <blockquote>
+	                  <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem&rdquo;</p>
+	                </blockquote>
+	                <div class="author d-flex">
+	                  <div class="image mr-3 align-self-center">
+	                    <div class="img" style="background-image: url(images/doctor-4.jpg);"></div>
+	                  </div>
+	                  <div class="name align-self-center">Aldin Powell <span class="position">Pulmonologist</span></div>
+	                </div>
+	              </div>
+	            </div>
+	          </div> <!-- .flip-container -->
+	        </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col text-center">
+            <div class="block-27">
+              <ul>
+                <li><a href="#">&lt;</a></li>
+                <li class="active"><span>1</span></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&gt;</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    	</div>
+    </section>
+		
+<?php
+      include('common/footer.php');
+?>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+  <!-- Modal -->
+<?php
+      include('common/modal.php');
+?>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
