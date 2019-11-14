91e3d44ceaba326ca9e4b2797838603281c03198
rukhsana27 <36368163+rukhsana27@users.noreply.github.com>
2019-11-14 11:27:12 +0600

    admin file added

---
+++ mediment-admin/monster-html/common/side_nav.php
@@ -0,0 +1,27 @@
+        <aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li>
+                            <a href="index.php" class="waves-effect"><i class="fa fa-clock-o m-r-10" aria-hidden="true"></i>Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="pages-profile.php" class="waves-effect"><i class="fa fa-user m-r-10" aria-hidden="true"></i>Profile</a>
+                        </li>
+                        <li>
+                            <a href="registration.php" class="waves-effect"><i class="fa fa-columns m-r-10" aria-hidden="true"></i>Approve Registrations</a>
+                        </li>
+                        <li>
+                            <a href="pages-blank.php" class="waves-effect"><i class="fa fa-columns m-r-10" aria-hidden="true"></i>Blank Page</a>
+                        </li>
+                        <li>
+                            <a href="pages-error-404.php" class="waves-effect"><i class="fa fa-info-circle m-r-10" aria-hidden="true"></i>Error 404</a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
