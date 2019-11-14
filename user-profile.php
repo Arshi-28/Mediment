464c2cc823e34d3d480c127835ea9f47fb9cb8fa
areepatw96 <arefapatw96@gmail.com>
2019-11-15 03:23:45 +0600

    changes done

---
+++ user-profile.php
@@ -0,0 +1,131 @@
+<?php
+    include('common/html_head.php');
+?>
+
+        <?php
+      include('common/nav.php');
+        ?>  
+   
+   <body class="fix-header card-no-border">
+   
+    
+    <div id="main-wrapper">
+  
+   
+     <div class="page-wrapper">
+      <div class="container-fluid"> 
+      <div class="row page-titles">
+                    <div class="col-md-6 col-8 align-self-center">
+                        <h3 class="text-themecolor m-b-0 m-t-0">Profile</h3>
+                        
+                    </div>
+                </div>   
+    <div class="row">
+                    <!-- Column -->
+                    <div class="col-lg-4 col-xlg-3 col-md-5">
+                        <div class="card">
+                            <div class="card-block">
+                                <center class="m-t-30"> <img src="images/doctor-4.jpg" class="img-circle" width="150" />
+                                    <h3 class="card-title m-t-10">Arefa patwary</h3>
+                                    <h5 class="card-subtitle">User Profile</h5>
+                                    <div class="row text-center justify-content-md-center">
+                                        <div class="col-4"><a href="fonts/profile-icons/multiple-users-silhouette.svg" class="link"><i class="icon-people"></i> <font class="font-medium">254</font></a></div>
+                                        <div class="col-4"><a href="fonts/profile-icons/picture.svg" class="link"><i class="icon-picture"></i> <font class="font-medium">54</font></a></div>
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+    <div class="col-lg-8 col-xlg-9 col-md-7">
+                        <div class="card">
+                            <div class="card-block">
+                                <form class="form-horizontal form-material">
+                                    <div class="form-group">
+                                        <label class="col-md-12">First Name</label>
+                                        <div class="col-md-12">
+                                            <input type="text" placeholder="First Name" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Last Name</label>
+                                        <div class="col-md-12">
+                                            <input type="text" placeholder="Last Name" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                   
+                                    
+                                    <div class="form-group">
+                                        <label class="col-md-12">Phone No</label>
+                                        <div class="col-md-12">
+                                            <input type="tel" placeholder="123 456 7890" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <label class="col-md-12">Age</label>
+                                        <div class="col-md-12">
+                                            <input type="number" placeholder="age" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Occupation</label>
+                                        <div class="col-md-12">
+                                            <input type="text" placeholder="Occupation" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Weight</label>
+                                        <div class="col-md-12">
+                                            <input type="number" placeholder="weight" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-md-12">Gender</label>
+                                        <div class="col-md-12">
+                                            <input type="text" placeholder="Gender" class="form-control form-control-line">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                    <div class="form-group">
+                                        <div class="col-sm-6">
+                                            <button class="btn btn-success">Update Profile</button>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-6">
+                                            <button class="btn btn-success">Book Appontment</button>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div> 
+      
+                </div>
+                </div>
+    <?php
+      include('common/footer.php');
+?>
+   </div>
+   </div>
+   
+    <script src="../assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../assets/plugins/bootstrap/js/tether.min.js"></script>
+    <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.min.js"></script>
+    
+    <script src="../assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+   
+   </body>
+   
