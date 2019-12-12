16bfb92c49da37fb07ef212b77710b745574b4a5
salmanc2 <32732052+salmanc2@users.noreply.github.com>
2019-12-12 13:29:58 +0600

    salman

--- mediment/templates/common/navbar.php
+++ mediment/templates/common/navbar.php
@@ -7,7 +7,7 @@
        <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php">Home</a>
               </li>
               <li class="nav-item">
                     <a class="nav-link" href="#">About</a>
@@ -16,11 +16,9 @@
                    <a class="nav-link" href="doctor.php">Doctors</a>
               </li>
               <li class="nav-item">
-                   <a class="nav-link" href="#">Contact</a>
+                   <a class="nav-link" href="contact.php">Contact</a>
               </li>
-              <li class="nav-item">
-                   <a class="nav-link" href="#">User</a>
-              </li>
+              
               
           </ul>
     
---
+++ mediment/templates/contact.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Medico</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/doctor.css">
+    <link rel="stylesheet" href="css/circle.css">
+
+
+
+</head>
+<body>
+     
+    <header>
+      <?php
+        include('common/navbar.php');
+        ?>
+</header>
+   
+   <main>
+     
+       <div class="section-1">
+           <div class="container text-center">
+            
+             
+            
+              
+    
+    
+    
+    <div class="section-2">
+    
+        <div class="container">
+            <h1 style="padding-top:20px; margin-top: 20px; margin-bottom: 70px; text-align: center">Contact Us</h1>
+            <div class="row">
+                <div class="col-md-5">
+                    <p style="text-align: left; margin-top: 20px; font-size: 20px">We'd love your feedback</p>
+                    <p style="text-align: left; margin-top: 80px; font-size: 20px"> Dhaka, Bangadesh <br>
+                        Phone:12345223 <br>
+                        Address: 122 East Road.
+                    </p>
+                </div>
+                
+                <div class="col-md-7">
+                    
+                     <div class="container" style="margin-bottom: 30px">
+                          <form action="">
+
+                            <label for="fname">First Name</label>
+                            <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+                            <label for="Email">Email</label>
+                            <input type="text" id="lname" name="email" placeholder="Your email..">
+
+                    
+
+                            <label for="message">Message</label>
+                            <textarea name="message" placeholder="Write something.." style="height:200px"></textarea>
+
+                            <input type="submit" value="Submit">
+
+                          </form>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+    
+<footer class="page-footer font-small blue">
+
+  
+  <div class="footer-copyright text-center py-3">© Medico
+    <a href="https://mdbootstrap.com/education/bootstrap/"></a>
+  </div>
+  
+
+</footer>
+
+
+    
+   </main>
+    
+    
+    <script src="https://kit.fontawesome.com/d2f5a4ed19.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
