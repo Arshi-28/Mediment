ed6bb1d20af35e5883fe5ce4aca0532ad45f362f
rukhsana27 <36368163+rukhsana27@users.noreply.github.com>
2019-12-28 04:15:46 +0600

    Update appointment_process.php

--- mediment/templates/appointment_process.php
+++ mediment/templates/appointment_process.php
@@ -2,10 +2,10 @@
 include 'common/db_connection.php';
 $doctor = $_GET['id1'];
 $user = $_GET['id2'];
-$time = $_POST['date'];
-$date = $_POST['user-time'];
-
-$sql="INSERT INTO appointments(date,time,doctorid,userid) VALUES('$date','$time','$doctor','$user')";
+$time = $_POST['user-time'];
+$date = $_POST['date'];
+$newdate= date('Y-m-d', strtotime($date));
+$sql="INSERT INTO appointments(date,time,doctorid,userid) VALUES('$newdate','$time','$doctor','$user')";
 
 $result = mysqli_query($connect, $sql);
 if($result){
