98fba149403df763bc16e5f9cb2ef2ee81036e5a
rukhsana27 <36368163+rukhsana27@users.noreply.github.com>
2019-12-28 04:33:10 +0600

    Update listappointment.php

--- mediment/templates/listappointment.php
+++ mediment/templates/listappointment.php
@@ -3,10 +3,7 @@
 include('common/db_connection.php');
 
 
-$sql = "SELECT appointmentid, date, time FROM appointments";
 
-$search_result = mysqli_query($connect, $sql);
-$count = mysqli_num_rows($search_result);
  ?> 
 
 <head>
@@ -39,60 +36,21 @@ tr:hover {background-color:#f5f5f5;}
 
      
     <header>
-       <?php session_start(); ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="index.php">Medico</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse"></div>
-       <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                    <a class="nav-link" href="index.php">About</a>
-              </li>
-              <li class="nav-item">
-                   <a class="nav-link" href="doctor.php">Doctors</a>
-              </li>
-              <li class="nav-item">
-                   <a class="nav-link" href="listappointment.php">View your Appointments</a>
-              </li>
-              <li class="nav-item">
-                   <a class="nav-link" href="user_profile.php">Your Profile</a>
-              </li>
-              <li class="nav-item">
-                   <a class="nav-link" href="contact.php">Contact Us</a>
-              </li>
-              
-              
-          </ul>
-           <?php 
-           if(!isset($_SESSION['fullname'])){ ?>
-           <form action="" class="form-inline my-2 my-lg-0">
-            <button class="btn menu-right-btn border" >
-                <a href="user-registration.php">Register</a>                
-            </button>
-             <button class="btn menu-left-btn border" >
-                <a href="login1.php">Login</a>               
-            </button>
-        </form>
-           <?php
-           }else{
-            echo $_SESSION['fullname'];?>
-               <a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i></a>
-          <?php } ?>
-           
-     </div>
-    </nav>
+       <?php include 'common/navbar.php';?>
+        <?php
+        $id = $_SESSION['id'];
+        $sql = "SELECT appointmentid, date, time FROM appointments WHERE userid='$id' AND verified='1'";
+        $search_result = mysqli_query($connect, $sql);
+        $num_of_rows = mysqli_num_rows($search_result);
+        ?>
 </header>
    
    <main>
         <div class="container text-center">
        <h1 style="font-weight: 500">Your List of Appointments</h1><br>
-
+<?php
+                if($num_of_rows > 0){
+                ?>
 <table>
   <tr>
     <th>Doctor Name</th>
@@ -113,6 +71,16 @@ tr:hover {background-color:#f5f5f5;}
 
 </table>
 </div>
+       <?php 
+                }
+                else if($num_of_rows == 0){
+                        ?>
+                        <div class='alert alert-danger'>
+                                No data found.
+                         </div>
+                        <?php
+                        }
+                        ?>
 <footer class="page-footer font-small blue">
 
   
