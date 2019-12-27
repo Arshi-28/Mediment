7b8f9e9d185e7eb8b5d3500a9284b1a6ff18a327
Arshi-28 <siddiqui.promiti@northsouth.edu>
2019-12-28 01:27:39 +0600

    Specialization.php

---
+++ mediment/templates/specialization.php
@@ -0,0 +1,23 @@
+<?php
+
+include('common/db_connection.php');
+
+?> 
+<head>
+    <meta charset="UTF-8">
+    <title>Medico</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/doctor.css">
+    <link rel="stylesheet" href="css/circle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+</head>
+<body>
+
+  
+
+</body>
+</html>
